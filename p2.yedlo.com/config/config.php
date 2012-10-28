bdd5b7a84b8062956e5c7edf2cc42078e0a77976
Derek Aubie <derek.k.aubie@gmail.com>
2012-10-28 09:33:24 -0400

    edit to app name (typo)

--- p2.yedlo.com/config/config.php
+++ p2.yedlo.com/config/config.php
@@ -15,7 +15,7 @@ If you want a different default time format for this app, set it below
 
 
 # What is the name of this app?
-	define('APP_NAME', 'p2_yedlo.com');  
+	define('APP_NAME', 'p2_yedlo_com');  
 
 # When email is sent out from the server, where should it come from?
 # Ideally, this should match the domain name
